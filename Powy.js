05b04ac4f9b1d55a9f74f5ebd0829c52c515cac9
Galphie <algparis96@gmail.com>
2023-04-07 21:53:11 +0200

    Funcionamiento básico terminado (sin control de roles)

--- Powy.js
+++ Powy.js
@@ -1,9 +1,13 @@
+const moment = require('moment/moment');
 const Log = require('./log');
 
+moment.locale('es')
 // Lista de espera para los turnos
 let turnos = [];
+let canalAModerar;
 
 function init(client, config) {
+
     client.once('ready', () => {
         console.clear();
         Log.success(`Bot conectado como ${client.user.tag}`);
@@ -15,38 +19,63 @@ function init(client, config) {
             Log.info('Se ha recibido un mensaje');
             Log.showMessage(message.author.tag, message.content);
 
+            //Comando '!moderar', para establecer el canal en el que se va a necesitar moderación
             if (message.content === `${config.prefix}moderar`) {
-                Log.info(`Canal a moderar: #${message.channel.name}`);
-                message.channel.send(`Se ha establecido el canal #${message.channel.name} como canal a moderar`)
+                canalAModerar = message.channel;
+                Log.info(`Canal a moderar: #${canalAModerar.name}`);
+                canalAModerar.send(`Se ha establecido el canal ${canalAModerar} como canal a moderar`)
             }
 
+            //Comando '!turno', para pedir el turno (añadir usuario a la lista de espera)
             if (message.content === `${config.prefix}turno`) {
                 Log.info(`${message.author.tag} ha pedido turno`);
-                const authorVoiceChannel = message.member.voice.channel;
-                Log.info("authorVoiceChannel: ", authorVoiceChannel);
-                if (!authorVoiceChannel) {
-                    Log.warning('No está en un canal de voz')
-                    message.reply('Debes estar en un canal de voz para pedir un turno.');
-                    return;
-                }
-                if (turnos.includes(message.author.id)) {
+
+                if (turnos.find(turno => turno.usuario === message.author)) {
                     Log.warning('Usuario ya incluido en la lista')
                     message.reply('Ya pediste un turno.');
                     return;
                 }
-                turnos.push(message.author.id);
-                message.reply('Pedido de turno registrado.');
+                const nuevoTurno = {
+                    usuario: message.author,
+                    hora: moment()
+                };
+                turnos.push(nuevoTurno);
+                message.reply('Solicitud de turno registrada.');
             }
 
-            if (message.content === `${config.prefix}siguienteTurno`) {
+            //Comando '!siguiente', para cambiar de turno en la lista. Solo los que tengan rol 'Moderador' podrán usarlo
+            if (message.content === `${config.prefix}siguiente`) {
                 //TODO: controlar que solo pueda usarlo un moderador
-                turnos.shift();
-                const nextUser = client.users.cache.get(turnos[0]);
-                nextUser.send('Es tu turno');
+                if(canalAModerar) {
+                    if(turnos.length > 0) {
+                        const nextUser = turnos[0].usuario;
+                        canalAModerar.send(`${nextUser}, ¡es tu turno!`);
+                        turnos.shift();
+                    } else {
+                        message.reply("Vaya, parece que no hay nadie en la lista")
+                    }
+                } else {
+                    message.reply(`Parece que no hay ningún canal a moderar seleccionado. Usa el comando '!moderar' para seleccionar este`)
+                }
             }
 
+            //Comando '!listaTurnos', para ver la lista de espera
             if (message.content === `${config.prefix}listaTurnos`) {
-                message.reply(turnos.toString());
+                if (turnos.length > 0) {
+                    let respuesta = "**__Lista de espera__**\n\n";
+                    turnos.forEach(turno => {
+                        respuesta += `${turno.usuario} *(${turno.hora.fromNow()})*` + "\n"
+                    });
+                    message.reply(respuesta);
+                } else {
+                    message.reply("Vaya, parece que no hay nadie en la lista")
+                }
+            }
+
+            if (message.content === `${config.prefix}test`) {
+
+                message.reply(canalAModerar.name);
+                canalAModerar.send("funciona")
             }
         }
     });
--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const client = new Client({
     ]
 });
 
-
 Powy.init(client, config);
 
 client.login(config.token);
